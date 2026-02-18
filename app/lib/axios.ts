5f7a68cedab3073b74b55d35e648ad772dcc36cb
ariffaysal <ariffaysal001@gmai.com>
2026-02-18 18:24:10 +0600

    m

--- app/lib/axios.ts
+++ app/lib/axios.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001', 
+  baseURL: 'http://127.0.0.1:3001', 
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default api;
+
+
+
