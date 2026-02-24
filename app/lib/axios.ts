465c6eb7782c8a24e36f1e5ea433f33b04858d57
ariffaysal <ariffaysal001@gmai.com>
2026-02-24 15:30:17 +0600

    b

--- app/lib/axios.ts
+++ app/lib/axios.ts
@@ -4,10 +4,9 @@ const api = axios.create({
   baseURL: 'http://127.0.0.1:3001', 
   headers: {
     'Content-Type': 'application/json',
+   
+    'x-api-key': '1234567890abcdef', 
   },
 });
 
 export default api;
-
-
-
